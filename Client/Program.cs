88787bf4fe86eba8e641bff01760cde5f27ca752
De0jbc <79905978+De0jbc@users.noreply.github.com>
2023-02-09 12:25:20 +0100

    Adding Orleans PingPong

---
+++ Client/Program.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using GrainInterfaces;
+using Orleans;
+using Client;
+
+
+
+try
+{
+    using IHost host = await StartClientAsync();
+    var client = host.Services.GetRequiredService<IClusterClient>();  
+        await DoClientWorkAsync(client);
+    
+    Console.ReadKey();
+    await host.StopAsync();
+
+    return 0;
+}
+catch (Exception e)
+{
+    Console.WriteLine($$"""
+        Exception while trying to run client: {{e.Message}}
+        Make sure the silo the client is trying to connect to is running.
+        Press any key to exit.
+        """);
+
+    Console.ReadKey();
+    return 1;
+}
+
+static async Task<IHost> StartClientAsync()
+{
+    var builder = new HostBuilder()
+        .UseOrleansClient(client =>
+        {
+            client.UseLocalhostClustering();
+        })
+        .ConfigureLogging(logging => logging.AddConsole());
+
+    var host = builder.Build();
+    await host.StartAsync();
+
+    Console.WriteLine("Client successfully connected to silo host \n");
+
+    return host;
+}
+
+static async Task DoClientWorkAsync(IClusterClient client)
+{
+    int numberOfClients;
+    int numberOfRepeatsPerClient;
+
+    
+
+    Benchmark benchmark;
+
+
+    
+        Console.WriteLine("Make sure that local silo is started. Press Enter to proceed ...");
+        Console.ReadLine();
+
+        Console.Write("Enter number of clients (thousands): ");
+        numberOfClients = int.Parse(Console.ReadLine() ?? Environment.ProcessorCount.ToString("D"));
+
+        Console.Write("Enter number of repeated pings per client (thousands): ");
+        numberOfRepeatsPerClient = int.Parse(Console.ReadLine() ?? "15");
+    
+        benchmark = new Benchmark(numberOfClients*1000, numberOfRepeatsPerClient*1000, client, client, client);
+        benchmark.Run();
+
+}
---
+++ Client/benchmark.cs
@@ -0,0 +1,147 @@
+﻿using GrainInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Threading.Tasks;
+
+namespace Client;
+
+    public class Benchmark
+    {
+        const string resultsFile = @"C:\Temp\OrleansPingPong.txt";
+
+        readonly List<IClient> clients = new List<IClient>();
+        readonly List<IClientObserver> observers = new List<IClientObserver>();
+
+        int numberOfClients;
+        int numberOfRepeatsPerClient;
+        IClusterClient ClientFactory;
+        IClusterClient DestinationFactory;
+        IClusterClient ClientObserverFactory;
+
+    public void SetBenchmark(int numberOfClients, int numberOfRepeatsPerClient, IClusterClient ClientFactory, IClusterClient DestinationFactory, IClusterClient ClientObserverFactory)
+    {
+        this.numberOfClients = numberOfClients;
+        this.numberOfRepeatsPerClient = numberOfRepeatsPerClient;
+        this.ClientFactory = ClientFactory;
+        this.DestinationFactory = DestinationFactory;
+        this.ClientObserverFactory = ClientObserverFactory;
+
+    }
+    public Benchmark(int numberOfClients, int numberOfRepeatsPerClient,IClusterClient ClientFactory, IClusterClient DestinationFactory, IClusterClient ClientObserverFactory)
+        {
+            this.numberOfClients = numberOfClients;
+            this.numberOfRepeatsPerClient = numberOfRepeatsPerClient;
+            this.ClientFactory = ClientFactory;
+            this.DestinationFactory = DestinationFactory;
+            this.ClientObserverFactory = ClientObserverFactory;
+
+    }
+
+        public async void Run()
+        {
+
+       
+         
+            var results = new List<Task<ClientResult>>();
+
+            for (var i = 0; i < numberOfClients; i++)
+            {
+                var destination = DestinationFactory.GetGrain<IDestination>(0);
+                var client = ClientFactory.GetGrain<IClient>(0);
+
+                await client.Initialize(destination, numberOfRepeatsPerClient);
+
+                var observer = new ClientObserver();
+                await client.Subscribe(ClientObserverFactory.CreateObjectReference<IClientObserver>(observer));
+
+                clients.Add(client);
+                observers.Add(observer);
+                results.Add(observer.AsTask());
+            
+        }
+            Console.WriteLine("Init. Done!.");
+            clients.ForEach(c => c.Run());
+            var stopwatch = Stopwatch.StartNew();
+            await Task.WhenAll(results.ToArray());
+            stopwatch.Stop();
+
+            WriteResultsToConsole(stopwatch);
+            WriteResultsToFile(stopwatch);
+
+            Console.WriteLine();
+            Console.WriteLine("Done!. Press any key to exit ...");
+            Console.ReadLine();
+        
+        
+    }
+
+        void WriteResultsToConsole(Stopwatch stopwatch)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            WriteResults(stopwatch, Console.Out);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        void WriteResultsToFile(Stopwatch stopwatch)
+        {
+            using (var writer = File.AppendText(resultsFile))
+                WriteResults(stopwatch, writer);
+        }
+
+        void WriteResults(Stopwatch stopwatch, TextWriter writer)
+        {
+            var totalActorCount = numberOfClients * 2L; // ping and pong actor
+            var totalMessagesReceived = numberOfRepeatsPerClient * totalActorCount * 2; // communication in Orleans' is always two-way
+            var throughput = (totalMessagesReceived / stopwatch.Elapsed.TotalSeconds);
+
+            writer.WriteLine("OSVersion: {0}", Environment.OSVersion);
+            writer.WriteLine("ProcessorCount: {0}", Environment.ProcessorCount);
+            writer.WriteLine("ClockSpeed: {0} MHZ", CpuSpeed());
+            writer.WriteLine();
+            writer.WriteLine("Number of Clients: {0}", numberOfClients);
+            writer.WriteLine("Number of Repeats per Client: {0}", numberOfRepeatsPerClient);
+            writer.WriteLine("Actor Count: {0}", totalActorCount);
+            writer.WriteLine("Total: {0:N} messages", totalMessagesReceived);
+            writer.WriteLine("Time: {0:F} sec", stopwatch.Elapsed.TotalSeconds);
+            writer.WriteLine("TPS: {0:##,###} per/sec", throughput);
+        }
+
+        static uint CpuSpeed()
+        {
+            using (var mo = new ManagementObject("Win32_Processor.DeviceID='CPU0'"))
+                return (uint)(mo["CurrentClockSpeed"]);
+        }
+    }
+
+    public class ClientObserver : IClientObserver
+    {
+        readonly TaskCompletionSource<ClientResult> tcs = new TaskCompletionSource<ClientResult>();
+
+    public void Done(long pings, long pongs)
+    {
+         
+             tcs.TrySetResult(new ClientResult(pings, pongs));
+           
+    }
+
+        public Task<ClientResult> AsTask()
+        {
+            return tcs.Task;
+        }
+    }
+
+    public class ClientResult
+    {
+        public readonly long Pings;
+        public readonly long Pongs;
+
+        public ClientResult(long pings, long pongs)
+        {
+            Pings = pings;
+            Pongs = pongs;
+        }
+    }
